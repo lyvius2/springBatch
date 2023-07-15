3717e4c9fe91470c45bdd703ef31f9d576d77cf6
walter.hwang <lyvius2@naver.com>
2023-07-16 02:12:41 +0900

    스프링부트 3.0으로 버전 업그레이드

--- src/main/java/com/walter/batch/job/HelloWorldJobConfig.java
+++ src/main/java/com/walter/batch/job/HelloWorldJobConfig.java
@@ -3,30 +3,32 @@ package com.walter.batch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class HelloWorldJobConfig {
-	private final JobBuilderFactory jobBuilderFactory;
-	private final StepBuilderFactory stepBuilderFactory;
+	private final JobRepository jobRepository;
+	private final PlatformTransactionManager transactionManager;
 
-	public HelloWorldJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-		this.jobBuilderFactory = jobBuilderFactory;
-		this.stepBuilderFactory = stepBuilderFactory;
+	public HelloWorldJobConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		this.jobRepository = jobRepository;
+		this.transactionManager = transactionManager;
 	}
 
 	@Bean
 	public Job helloWorldJob() {
-		return jobBuilderFactory.get("helloWorldJob")
+		return new JobBuilder("helloWorldJob", jobRepository)
 				.incrementer(new RunIdIncrementer())
 				.start(helloWorldStep())
 				.build();
@@ -35,8 +37,8 @@ public class HelloWorldJobConfig {
 	@JobScope
 	@Bean
 	public Step helloWorldStep() {
-		return stepBuilderFactory.get("helloWorldStep")
-				.tasklet(helloWorldTasklet())
+		return new StepBuilder("helloWorldStep", jobRepository)
+				.tasklet(helloWorldTasklet(), transactionManager)
 				.build();
 	}
 
