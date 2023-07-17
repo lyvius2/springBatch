05d4cde1490e7917fd8371510aaa16ba2d41d2a9
walter.hwang <lyvius2@naver.com>
2023-07-17 23:19:10 +0900

    parameter incrementer 추가

--- src/main/java/com/walter/batch/job/HelloWorldJobLazyConfig.java
+++ src/main/java/com/walter/batch/job/HelloWorldJobLazyConfig.java
@@ -1,10 +1,13 @@
 package com.walter.batch.job;
 
+import com.walter.batch.util.ParameterValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -15,6 +18,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Arrays;
 
 @Slf4j
 @Configuration
@@ -29,24 +34,38 @@ public class HelloWorldJobLazyConfig {
 
 	@Bean
 	public Job job() {
-		return new JobBuilder("helloWorldLazyJob", jobRepository).incrementer(new RunIdIncrementer())
+		return new JobBuilder("helloWorldLazyJob", jobRepository)
 																	   .start(step1())
+																	   .validator(validator())
+																	   .incrementer(new RunIdIncrementer())
 																	   .build();
 	}
 
 	@JobScope
 	@Bean
 	public Step step1() {
-		return new StepBuilder("step1", jobRepository).tasklet(helloWorldLazyTasklet(null), transactionManager)
+		return new StepBuilder("step1", jobRepository).tasklet(helloWorldLazyTasklet(null, null), transactionManager)
 															.build();
 	}
 
 	@StepScope
 	@Bean
-	public Tasklet helloWorldLazyTasklet(@Value("#{jobParameters['name']}") String name) {
+	public Tasklet helloWorldLazyTasklet(@Value("#{jobParameters['name']}") String name,
+	                                     @Value("#{jobParameters['fileName']}") String fileName) {
 		return ((contribution, chunkContext) -> {
 			log.info("Hello, {}", name);
+			log.info("fileName = {}", fileName);
 			return RepeatStatus.FINISHED;
 		});
+	}
+
+	@Bean
+	public CompositeJobParametersValidator validator() {
+		final CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+		final DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(new String[]{"fileName"}, new String[]{"name", "run.id"});
+		defaultJobParametersValidator.afterPropertiesSet();
+		validator.setValidators(Arrays.asList(new ParameterValidator(), defaultJobParametersValidator));
+		return validator;
 	}
 }
