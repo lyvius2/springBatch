94d1c687d7b6b0b51141a8f85ab929d07233722d
walter.hwang <lyvius2@naver.com>
2023-07-22 20:51:51 +0900

    method invoking tasklet 예제 (parameter 수신 추가)

--- src/main/java/com/walter/batch/job/MethodInvokingJobConfig.java
+++ src/main/java/com/walter/batch/job/MethodInvokingJobConfig.java
@@ -3,12 +3,12 @@ package com.walter.batch.job;
 import com.walter.batch.util.PojoService;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -31,15 +31,17 @@ public class MethodInvokingJobConfig {
 
 	@Bean
 	public Step methodInvokingStep() {
-		return new StepBuilder("methodInvokingStep", jobRepository).tasklet(methodInvokingTasklet(), transactionManager)
+		return new StepBuilder("methodInvokingStep", jobRepository).tasklet(methodInvokingTasklet(null), transactionManager)
 																		 .build();
 	}
 
+	@StepScope
 	@Bean
-	public MethodInvokingTaskletAdapter methodInvokingTasklet() {
+	public MethodInvokingTaskletAdapter methodInvokingTasklet(@Value("#{jobParameters['message']}") String message) {
 		final MethodInvokingTaskletAdapter adapter = new MethodInvokingTaskletAdapter();
 		adapter.setTargetObject(pojoService());
 		adapter.setTargetMethod("main");
+		adapter.setArguments(new String[] {message});
 		return adapter;
 	}
 
--- src/main/java/com/walter/batch/util/PojoService.java
+++ src/main/java/com/walter/batch/util/PojoService.java
@@ -4,4 +4,8 @@ public class PojoService {
 	public void main() {
 		System.out.println("Main method is called");
 	}
+
+	public void main(String message) {
+		System.out.println("Main method's reference value : " + message);
+	}
 }
