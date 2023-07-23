568c756d0035fb906642e3edba8159aa6420ca3b
walter.hwang <lyvius2@naver.com>
2023-07-23 21:31:51 +0900

    job status로 조건 로직 구현 샘플 추가

--- src/main/java/com/walter/batch/job/ConditionalJobConfig.java
+++ src/main/java/com/walter/batch/job/ConditionalJobConfig.java
@@ -3,7 +3,6 @@ package com.walter.batch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
